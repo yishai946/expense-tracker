cae3a26dd85f32a00cdf2ef159fe8c323241ac12
Yishai Biton <yishai946@gmail.com>
2024-01-25 18:15:32 +0200

    try fix cors

--- api/app/index.js
+++ api/app/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors({
   origin: 'http://localhost:5173',
-  credentials: true,
+//   credentials: true,
   accessControlAllowOrigin: '*',
 }));
 app.use("/api/users", usersRouter);
