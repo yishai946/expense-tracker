da980e268ac9b90e7739bf7a6efdb8264c07cee0
Yishai Biton <yishai946@gmail.com>
2024-01-27 23:40:11 +0200

    try fix cors

--- api/app/index.js
+++ api/app/index.js
@@ -8,25 +8,31 @@ const cors = require("cors");
 
 const app = express();
 
-app.options("*", function (req, res) {
-  res.sendStatus(200);
-});
+app.options("*", cors()); // Handle OPTIONS requests with cors middleware
 
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://finance-tracker-client-psi.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    preflightContinue: false,
   })
 );
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://finance-tracker-client-psi.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "*")
+app.use(function (req, res, next) {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://finance-tracker-client-psi.vercel.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Specify allowed methods
 
   if (req.method === "OPTIONS") {
-    res.sendStatus(200);
+    return res.sendStatus(200); // Respond with 200 OK for preflight requests
   }
 
   next();
