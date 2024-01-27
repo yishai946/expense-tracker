54f67b97e2f851c761d774aa72e2d655c87ef2b8
Yishai Biton <yishai946@gmail.com>
2024-01-28 01:14:29 +0200

    try fix cors

--- api/app/index.js
+++ api/app/index.js
@@ -8,43 +8,30 @@ const cors = require("cors");
 
 const app = express();
 
-app.options("*", (req, res) => {
-  console.log("OPTIONS activated");
-  res.sendStatus(200);
-} );
-
-app.get("/api", (req, res) => {
-  res.send("Hello, world!");
-});
-
-app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ["*"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    preflightContinue: false,
-  })
-);
-
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://finance-tracker-client-psi.vercel.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, application/json"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Specify allowed methods
+const corsMiddleware = (req, res, next) => {
+  res
+    .header("Access-Control-Allow-Origin", "*")
+    .header(
+      "Access-Control-Allow-Headers",
+      "Authorization,Accept,Origin,DNT,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range"
+    )
+    .header(
+      "Access-Control-Allow-Methods",
+      "GET,POST,OPTIONS,PUT,DELETE,PATCH"
+    );
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200); // Respond with 200 OK for preflight requests
+    res.sendStatus(200);
+  } else {
+    next();
   }
+};
 
-  next();
-});
+app.use(corsMiddleware);
 
-// app.use("/api/users", usersRouter);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/api/users", usersRouter);
 app.use("/api/expenses", expensesRouter);
 app.use("/api/incomes", incomesRouter);
 app.use("/api/home", homeRouter);
