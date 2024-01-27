42b7bc2cda9191b6bcee5800ef67356dabcd5a82
Yishai Biton <yishai946@gmail.com>
2024-01-28 01:43:16 +0200

    try fix cors

--- api/app/index.js
+++ api/app/index.js
@@ -18,6 +18,10 @@ app.use(cors({
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/api", cors(), (req, res) => {
+  res.send("Hello World!");
+});
+
 app.use("/api/users", usersRouter);
 app.use("/api/expenses", expensesRouter);
 app.use("/api/incomes", incomesRouter);
