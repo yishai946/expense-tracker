e71efbe3e830111f8673c9dda1363b6921e4b1f1
Yishai Biton <yishai946@gmail.com>
2024-10-06 10:46:17 +0300

    try fix server

--- api/index.js
+++ api/index.js
@@ -1,21 +1,28 @@
-const app = require("./app");
+const app = require("./app"); // Express app
+const serverless = require("serverless-http");
 require("dotenv").config();
 const MongoDB = require("./app/db/mongodb");
 
-const run = async () => {
-    try{
-        const port  = process.env.PORT || 5000;
-        await MongoDB.instance().connect();
-        app.listen(port, () => console.log(`Listening on port: ${port}`));
-    }
-    catch(err){
-        console.error("Failed to start: ", err);
-    }
-}
+let isDbConnected = false;
 
-run();
+const connectToDatabase = async () => {
+  if (!isDbConnected) {
+    await MongoDB.instance().connect();
+    isDbConnected = true;
+  }
+};
 
-process.on("SIGINT", async () => {
-  await MongoDB.instance().disconnect();
-  process.exit(0);
-});
+// Lambda handler
+module.exports.handler = async (event, context) => {
+  try {
+    await connectToDatabase();  // Ensure the database is connected before handling the request
+    const handler = serverless(app);  // Wrap your Express app
+    return await handler(event, context);  // Delegate the request to the Express app
+  } catch (err) {
+    console.error("Failed to process request: ", err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+};
